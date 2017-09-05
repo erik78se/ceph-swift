73391dbf7124f150b2e53aa7cf084d31725cca15
Erik Lönroth <erik.lonroth@gmail.com>
2017-09-05 20:57:29 +0200

    more

---
+++ env.sh
@@ -0,0 +1,11 @@
+export ST_AUTH_VERSION=3
+export OS_USERNAME=testuser
+export OS_USER_DOMAIN_NAME=admin_domain
+export OS_PASSWORD=1234
+export OS_PROJECT_NAME=admin
+export OS_PROJECT_DOMAIN_NAME=admin_domain
+export OS_AUTH_URL=http://192.168.1.173:5000/v3
+
+
+# swift list
+
